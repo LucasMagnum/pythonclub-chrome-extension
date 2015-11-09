b8b5e70b9a530709b6d6dc1999c8d6bc7c8689a9
Lucas Magnum <lucasmagnum2@gmail.com>
2015-11-09 00:46:25 -0200

    Mostrando link para todos os posts

--- static/js/pythonclub.js
+++ static/js/pythonclub.js
@@ -1,4 +1,28 @@
-function showPosts(postsHtml){}
+function showPost(post){
+
+    var postJSON = JSON.parse(post[0]);
+
+    var postTemplate = "<a class='post-link' href='" + postJSON.url + "' target='_blank'>" + postJSON.title + "</a>";
+
+    var postList = document.getElementById('post-list');
+
+    postList.innerHTML = postList.innerHTML + postTemplate;
+
+}
+
+function showPosts(pageHtml){
+    var postsRegex = new RegExp(/{"url": "[-a-zA-Z0-9@:%_\+.~#?&\/\/=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&\/\/=]*)", "title": "((?:\\.|[^"\\])*)"}/g);
+
+    var post;
+
+    while ((post = postsRegex.exec(pageHtml)) !== null) {
+        if (post.index === postsRegex.lastIndex) {
+            postsRegex.lastIndex++;
+        }
+        showPost(post);
+    }
+
+}
 
 function showFailed(){}
 
