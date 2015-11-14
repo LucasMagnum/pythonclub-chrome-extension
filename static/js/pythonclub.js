552481a809856274142ddf0ec6ba221898283d7f
Lucas Magnum <lucasmagnum2@gmail.com>
2015-11-13 23:15:00 -0200

    Docstring para funções

--- static/js/pythonclub.js
+++ static/js/pythonclub.js
@@ -2,6 +2,10 @@ var posts = [];
 var postList = document.getElementById('post-list');
 
 function showPosts(){
+    /*
+        Percorre o array `posts` e mostra dentro do elemento `postList`
+        o link para cada `post` parseado.
+    */
     postList.innerHTML = '';
 
     for (var i=0; i<posts.length; i++){
@@ -13,6 +17,10 @@ function showPosts(){
 }
 
 function getPosts(){
+    /*
+        Faz uma requisição para o PythonClub e mostra todos
+        os posts ou uma mensagem de erro.
+    */
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange=function() {
         if (xhttp.readyState == 4 && xhttp.status == 200) {
@@ -28,7 +36,11 @@ function getPosts(){
 }
 
 function savePosts(pageHtml){
-    var postsRegex = new RegExp(/{"url": "[-a-zA-Z0-9@:%_\+.~#?&\/\/=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&\/\/=]*)", "title": "((?:\\.|[^"\\])*)"}/g);
+    /*
+        Realiza o parser de todos os posts do PythonClub e insere
+        no array `posts`
+    */
+    var postsRegex = /{"url": "[-a-zA-Z0-9@:%_\+.~#?&\/\/=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&\/\/=]*)", "title": "((?:\\.|[^\"\\])*)"}/g;
     var post;
 
     while ((post = postsRegex.exec(pageHtml)) !== null) {
@@ -43,6 +55,7 @@ function savePosts(pageHtml){
 }
 
 function showFailedBox(){
+    /* Mostra a mensagem de erro com um link para reload */
     var failedBox = document.getElementById('failed-box');
 
     failedBox.style.visibility = "visible";
@@ -53,7 +66,6 @@ function showFailedBox(){
         window.location.reload();
     }
 }
-
 
 window.onload = getPosts;
 
